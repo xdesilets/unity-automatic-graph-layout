b1930a285f6495c771831b6ce0e1d78c254655e9
Xavier Désilets <xavier.desilets@unity3d.com>
2025-02-18 10:25:47 -0500

    Removing all unused stuffs

--- GraphLayout/NuGet/build_nuget_packages.ps1
+++
@@ -1,7 +0,0 @@
-.\AcquireNuGetExe.ps1
-
-Push-Location $PSScriptRoot 
-nuget pack Microsoft.Msagl.2024.nuspec
-nuget pack Microsoft.Msagl.GraphViewerGDI.nuspec
-nuget pack Microsoft.Msagl.WpfGraphControl.nuspec
-Pop-Location
